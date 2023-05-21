97364a55051c581378457e9459035ff7891fc7b9
AsimaTitanicCoder <asimamyrzabay@example.com>
2023-05-22 02:06:28 +0900

    first commit

--- src/BST.java
+++ src/BST.java
@@ -1,29 +1,139 @@
-java.util.ArrayList;
-public class BST {
-}
-public class BST<K extends Comparable<K>, V> {
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class BST<K extends Comparable<K>, V> implements Iterable<BST.Entry<K, V>> {
+
     private Node root;
-    private class Node
-    {
-        private  K key;
+    private class Node {
+        private K key;
         private V val;
         private Node left, right;
-        public Node(K key, V val)
-        {
+
+        public Node(K key, V val) {
             this.key = key;
             this.val = val;
         }
-        public void put(K key , V val) {
+    }
 
+    public void put(K key, V val) {
+        root = put(root, key, val);
+    }
+
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
+            return new Node(key, val);
         }
-        public V get(K key, V val) {
 
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, val);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, val);
+        } else {
+            node.val = val;
         }
-        public void delete(K key) {
+        return node;
+    }
 
+    public V get(K key) {
+        Node node = get(root, key);
+        return node == null ? null : node.val;
+    }
+
+    private Node get(Node node, K key) {
+        if (node == null) {
+            return null;
         }
-        public Iterable<K> iterator() {
 
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return get(node.left, key);
+        } else if (cmp > 0) {
+            return get(node.right, key);
+        } else {
+            return node;
         }
     }
+
+    public void delete(K key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = delete(node.left, key);
+        } else if (cmp > 0) {
+            node.right = delete(node.right, key);
+        } else {
+            if (node.left == null) {
+                return node.right;
+            } else if (node.right == null) {
+                return node.left;
+            } else {
+                Node successor = findMin(node.right);
+                node.key = successor.key;
+                node.val = successor.val;
+                node.right = deleteMin(node.right);
+            }
+        }
+        return node;
+    }
+
+    private Node findMin(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return findMin(node.left);
+    }
+
+    private Node deleteMin(Node node) {
+        if (node.left == null) {
+            return node.right;
+        }
+        node.left = deleteMin(node.left);
+        return node;
+    }
+
+    @Override
+    public Iterator<Entry<K, V>> iterator() {
+        ArrayList<Entry<K, V>> entries = new ArrayList<>();
+        inorderTraversal(root, entries);
+        return entries.iterator();
+    }
+
+    private void inorderTraversal(Node node, ArrayList<Entry<K, V>> entries) {
+        if (node == null) {
+            return;
+        }
+
+        inorderTraversal(node.left, entries);
+        entries.add(new Entry<>(node.key, node.val));
+        inorderTraversal(node.right, entries);
+    }
+
+    public static class Entry<K, V> {
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        BST<Integer, String> tree = new BST<>();
+        tree.put(1, "One");
+        tree.put(2, "Two");
+        tree.put(3, "Three");
+        tree.put(4,"ali");
+        tree.put(5,"asi");
+
+        for (BST.Entry<Integer, String> entry : tree) {
+            System.out.println("key is " + entry.getKey() + " and value is " + entry.getValue());
+        }
+
+    }}
